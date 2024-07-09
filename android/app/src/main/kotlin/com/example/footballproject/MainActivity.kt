61fbccd118a8881b3acb49a37802203e1b42a8dd
ayouboueslati <ayoubweslati00@gmail.com>
2024-07-09 17:21:22 +0100

    for apk

--- android/app/src/main/kotlin/com/example/footballproject/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/footballproject/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.footballproject
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCenter.start(application, ""4ef81e71-8921-4b21-bf23-37b2fdee74fe"", Analytics::class.java, Crashes::class.java)
+    }
+}
