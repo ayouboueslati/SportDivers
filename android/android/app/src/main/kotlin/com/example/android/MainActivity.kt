77c3744241ff73fbc2610a15b297d831f502623f
ayouboueslati <ayoubweslati00@gmail.com>
2024-07-10 14:19:48 +0100

    fixing mainactivity.kt

--- android/android/app/src/main/kotlin/com/example/android/MainActivity.kt
+++ android/android/app/src/main/kotlin/com/example/android/MainActivity.kt
@@ -2,4 +2,9 @@ package com.example.android
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCenter.start(application, "5d6d517a-f682-404f-aefe-c22577cb8db6", Analytics::class.java, Crashes::class.java)
+    }
+}
