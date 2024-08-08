037a8eb9ec16046e8f212587cfc5a50511db9042
ayouboueslati <ayoubweslati00@gmail.com>
2024-08-08 12:34:18 +0100

    changes in android folder

--- lib/models/ticket.dart
+++ lib/models/ticket.dart
@@ -1,23 +1,31 @@
 // lib/models/ticket_model.dart
 
+import 'package:footballproject/models/user.dart';
+
 class Ticket {
   final String id;
+  final DateTime createdAt;
   final String reason;
   final String comment;
-  final String target;
+  final bool isAdminTarget;
+  final User target;
 
   Ticket({
     required this.id,
+    required this.createdAt,
     required this.reason,
     required this.comment,
+    required this.isAdminTarget,
     required this.target,
   });
 
   factory Ticket.fromJson(Map<String, dynamic> json) {
     return Ticket(
-      id: json['id'] ?? '', // Adjust based on your actual response
+      id: json['id'] ?? '',
+      createdAt: json['createdAt'], // Adjust based on your actual response
       reason: json['reason'],
       comment: json['comment'],
+      isAdminTarget: json['isAdminTarget'],
       target: json['target'],
     );
   }
@@ -25,8 +33,10 @@ class Ticket {
   Map<String, dynamic> toJson() {
     return {
       'id': id,
+      'createdAt': createdAt,
       'reason': reason,
       'comment': comment,
+      'isAdminTarget': isAdminTarget,
       'target': target,
     };
   }
